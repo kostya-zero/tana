0ac4b6937a40340ab211a7a4d69d40aae85d87d9
Konstantin Zhigaylo <zero@kostyazero.com>
2025-09-01 14:24:55 +0300

    feat: logic for store

--- store.go
+++ store.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/gob"
+	"errors"
 	"os"
 )
 
@@ -43,4 +44,46 @@ func LoadFromFile(path string, store *Store) error {
 	}
 
 	return nil
+}
+
+func (s *Store) Set(key, value string) error {
+	_, ok := s.store[key]
+	if ok {
+		return errors.New("key already exists")
+	}
+
+	s.store[key] = value
+	return nil
+}
+
+func (s *Store) Get(key string) (string, error) {
+	value, ok := s.store[key]
+	if !ok {
+		return "", errors.New("key not found")
+	}
+	return value, nil
+}
+
+func (s *Store) Update(key, newValue string) error {
+	_, ok := s.store[key]
+	if !ok {
+		return errors.New("key not found")
+	}
+
+	s.store[key] = newValue
+	return nil
+}
+
+func (s *Store) Delete(key string) error {
+	_, ok := s.store[key]
+	if !ok {
+		return errors.New("key not found")
+	}
+
+	delete(s.store, key)
+	return nil
+}
+
+func (s *Store) GetAll() *map[string]string {
+	return &s.store
 }
